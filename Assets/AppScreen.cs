288b969fc0dfb0dca8d3e6814f7e4b46846b31b8
Keenan Gray <keenangray@Keenans-MacBook-Pro.local>
2018-10-16 22:28:00 -0400

    updated screen transition

--- Assets/AppScreen.cs
+++ Assets/AppScreen.cs
@@ -6,20 +6,30 @@ using UnityEngine.UI;
 public class AppScreen : MonoBehaviour {
 
     RectTransform rt;
+    GameObject MainCanvas;
 
     float timeOfTravel = 5; //time after object reach a target place 
     float currentTime = 0; // actual floting time 
     float normalizedValue;
+
+    float rate = 1.00f;
 
     Button close_button;
 
     // Use this for initialization
     void Start () {
         //TODO: prevent hardcoding of value here, use screen width insteads
-        Debug.Log("Screen Width " + ResolutionManager.GetCurrentResolution().width);
+        MainCanvas = GameObject.Find("AppCanvas");
+
+        if(MainCanvas!=null){
+            Debug.LogWarning("App Canvas Not Found");
+        }
+
+        Debug.Log("Screen Width " + MainCanvas.GetComponent<RectTransform>().rect.width);
+        var Res = MainCanvas.GetComponent<RectTransform>().rect.width;
 
         rt = GetComponent<RectTransform>();
-        rt.anchoredPosition = new Vector3(1334, 0, 0);
+        rt.anchoredPosition = new Vector3(Res, 0, 0);
 
         //assign the close_button
         //gameobject must be named close_button and be a child of this gameobject
@@ -56,14 +66,16 @@ public class AppScreen : MonoBehaviour {
     //TODO:Fix the coroutine not getting to 'yield break' fast enough
     IEnumerator MoveScreenIn()
     {
+        float lerp = 0;
+
         currentTime = 0;
         while (currentTime <= timeOfTravel)
         {
             currentTime += Time.deltaTime;
             normalizedValue = currentTime / timeOfTravel; // we normalize our time 
 
-            rt.anchoredPosition = Vector3.Lerp(rt.anchoredPosition, new Vector3(0, 0, 0), normalizedValue);
-
+            rt.anchoredPosition = Vector3.Lerp(rt.anchoredPosition, new Vector3(0, 0, 0), lerp);
+            lerp += rate;
             if (rt.anchoredPosition == new Vector2(0, 0))
             {
                 break;
@@ -77,14 +89,16 @@ public class AppScreen : MonoBehaviour {
 
     IEnumerator MoveScreenOut()
     {
+        float lerp = 0;
+
         close_button.gameObject.SetActive(false);
         currentTime = 0;
         while (currentTime <= timeOfTravel)
         {
-            currentTime += Time.deltaTime;
-            normalizedValue = currentTime / timeOfTravel; // we normalize our time 
+           
 
-            rt.anchoredPosition = Vector3.Lerp(rt.anchoredPosition, new Vector3(1334, 0, 0), normalizedValue);
+            rt.anchoredPosition = Vector3.Lerp(rt.anchoredPosition, new Vector3(1334, 0, 0), lerp);
+            lerp += rate;
 
             if (rt.anchoredPosition == new Vector2(1334, 0)){
                 break;
