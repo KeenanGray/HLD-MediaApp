808e47e1ba3e207862f2824652e4874e383c6d3e
Keenan Gray <keenangray@Keenans-MacBook-Pro-2.local>
2019-02-24 12:27:46 -0500

    added file for accessible instructins

---
+++ Assets/AssignAccessibleInstructions.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UI_Builder;
+
+public class AssignAccessibleInstructions : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<UIB_Button>().Button_Opens = UIB_Button.UIB_Button_Activates.Accessibletext;
+        GetComponent<UIB_Button>().s_link = "This app provides several accessibility features. You can explore by touch, or swipe left and right to navigate menus. Swiping up and down will" +
+            "jump between groups of items. ";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
