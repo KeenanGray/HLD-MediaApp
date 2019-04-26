4f0eeb2268293a68639b467f4d4425b2c53a611f
Keenan Gray <keenangray@Keenans-MacBook-Pro-2.local>
2019-04-25 20:15:17 -0400

    ressampled audio for smaller file sizes

---
+++ Assets/AssignUnfinishedFeedbackLink.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UI_Builder;
+using UnityEngine;
+
+public class AssignUnfinishedFeedbackLink : MonoBehaviour
+{
+    // Start is called before the first frame update
+    // Start is called before the first frame update
+    void Start()
+    {
+        var b = GetComponent<UIB_Button>();
+        b.Button_Opens = UIB_Button.UIB_Button_Activates.InAppUrl;
+        b.Title = "D.I.S.P.L.A.Y.E.D Feedback";
+        b.s_link = "https://forms.gle/AGpPbe3AVzUhSgsq9";
+    }
+
+}
